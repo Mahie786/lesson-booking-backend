c8e902f6f1a62e3fa0ad975bc3a97018154d7bd4
Mahie786 <mahierahman@outlook.com>
2024-12-07 23:56:13 +0500

    fix: lesson space on order success

--- src/services/order.service.js
+++ src/services/order.service.js
@@ -1,8 +1,18 @@
 const { getDB } = require("../../config/database");
+const { ObjectId } = require("mongodb");
 
 const insertOrder = async (order) => {
+  console.log("orders:", order)
   const db = getDB();
   const result = await db.collection("orders").insertOne(order);
+
+    // Update spaces for each lesson in the order
+  for (const lesson of order.lessons) {
+    await db.collection("lessons").updateOne(
+      { _id: new ObjectId(lesson.lessonId) },
+      { $inc: { spaces: -lesson.spaces } }
+    );
+  }
 
   // Attach the insertedId to the lesson object
   const insertedOrder = { ...order, _id: result.insertedId };
